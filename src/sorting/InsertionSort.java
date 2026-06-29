b454689a86f800ecfa983b52c3a2719438922ce5
Punith0912 <prajwalpraju09122001@gmail.com>
2026-06-29 22:28:31 +0530

    Insertion Sort

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements greater than key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // Insert key at correct position
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+    }
+}
