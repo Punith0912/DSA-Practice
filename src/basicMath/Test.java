0689b45e46ca29bfe73364b9af203f73b1333dfa
Punith0912 <prajwalpraju09122001@gmail.com>
2026-07-01 21:17:59 +0530

    merge Sort

--- src/basicMath/Test.java
+++ src/basicMath/Test.java
@@ -4,5 +4,17 @@ public class Test {
 
 // C:\Users\OO Manase\eclipse-workspace\Data Structure and Algorithm
 	
+	public static void main(String[] args) {
+		
+		f(3,3);
+	}
+
+	private static void f(int i, int j) {
+		if(i==0)
+			return;
+		
+		f(i-1,j);
+		System.out.println(i);
+	}
 }
 
