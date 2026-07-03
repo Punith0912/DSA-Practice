228e6d15ec09231a18b11b2143b54738909803fa
Punith0912 <prajwalpraju09122001@gmail.com>
2026-07-03 23:59:45 +0530

    check sorted

---
+++ src/arrays/easy/IsArraySorted.java
@@ -0,0 +1,22 @@
+package arrays.easy;
+
+public class IsArraySorted {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,3,4};
+		
+		System.out.println(isSorted(arr));
+	}
+
+	private static boolean isSorted(int[] arr) {
+
+		for(int i=0;i<arr.length-1;i++) {
+			
+			if(arr[i+1]< arr[i])
+				return false;
+		}
+		
+		return true;
+	}
+}
