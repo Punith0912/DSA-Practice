d33624b75e25a5589b486a5527a3316bf96103d5
Punith0912 <prajwalpraju09122001@gmail.com>
2026-07-06 23:53:04 +0530

    Quick Sort

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,17 @@
+package sorting;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {32,4,42,5,2,3};
+		
+		quickSort(arr);
+		
+	}
+
+	private static void quickSort(int[] arr) {
+		// TODO Auto-generated method stub
+		
+	}
+}
