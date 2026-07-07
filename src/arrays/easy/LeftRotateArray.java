c8012708e54443be3b46bd34ebf82b5608b6fb8b
Punith0912 <prajwalpraju09122001@gmail.com>
2026-07-07 20:59:59 +0530

    Rotate by K places

--- src/arrays/easy/LeftRotateArray.java
+++ src/arrays/easy/LeftRotateArray.java
@@ -7,7 +7,41 @@ public class LeftRotateArray {
 		
 		int[] arr= {1,2,3,4,5};
 		
-		rotateBy1(arr);
+		//rotateBy1(arr);
+		
+		int k =3;
+		rotateByK(arr, k);
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+	private static void rotateByK(int[] arr, int k) {
+		
+		// approach 1
+		int n= arr.length;
+		int temp[] =new int[k];
+		
+		for(int i =0;i<k;i++) {
+			temp[i]=arr[i];
+		}
+		
+		for(int i =k;i<n;i++) {
+			arr[i - k] = arr[i];
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		
+		for(int i =0; i< k;i++) {
+			
+			System.out.println(n-k+" "+temp[i]);
+			arr[n-k+i] = temp[i];
+			
+		}
 	}
 
 	private static void rotateBy1(int[] arr) {
@@ -22,4 +56,6 @@ public class LeftRotateArray {
 		// replacing the stored 1st element to the last index
 		arr[n-1]= temp;
 	}
+	
+	
 }
