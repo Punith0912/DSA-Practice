8af0f4849d64a57466037589c687fb2889552cff
Punith0912 <prajwalpraju09122001@gmail.com>
2026-07-08 23:32:10 +0530

    Optimal K rotation

--- src/arrays/easy/LeftRotateArray.java
+++ src/arrays/easy/LeftRotateArray.java
@@ -7,42 +7,16 @@ public class LeftRotateArray {
 		
 		int[] arr= {1,2,3,4,5};
 		
-		//rotateBy1(arr);
-		
-		int k =3;
-		rotateByK(arr, k);
+		rotateBy1(arr);
 		
 		for(int i=0;i<arr.length;i++) {
 			System.out.print(arr[i]+" ");
 		}
 	}
-
-	private static void rotateByK(int[] arr, int k) {
-		
-		// approach 1
-		int n= arr.length;
-		int temp[] =new int[k];
-		
-		for(int i =0;i<k;i++) {
-			temp[i]=arr[i];
-		}
-		
-		for(int i =k;i<n;i++) {
-			arr[i - k] = arr[i];
-		}
-		
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();
-		
-		for(int i =0; i< k;i++) {
-			
-			System.out.println(n-k+" "+temp[i]);
-			arr[n-k+i] = temp[i];
-			
-		}
-	}
+	
+	
+	
+	
 
 	private static void rotateBy1(int[] arr) {
 		// storing 1st element 
