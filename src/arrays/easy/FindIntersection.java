e4c5e9f0f95fe02bf81518fd87049165af5320ff
Punith0912 <prajwalpraju09122001@gmail.com>
2026-07-09 19:41:53 +0530

    Optimized Intersection of Two Sorted arrays

--- src/arrays/easy/FindIntersection.java
+++ src/arrays/easy/FindIntersection.java
@@ -15,36 +15,72 @@ public class FindIntersection {
 		System.out.println(intersection);
 	}
 
+//	Approach 1 
+	
+//	private static List<Integer> findIntersection(int[] arr1, int[] arr2) {
+//
+//		int n1 = arr1.length;
+//		int n2 = arr2.length;
+//
+//		int[] visited = new int[n2];
+//
+//		List<Integer> intersection = new ArrayList<Integer>();
+//
+//		for(int i =0 ;i<n1 ;i++) {
+//
+//			for(int j =0; j<n2 ; j++) {
+//
+//				if(arr1[i] == arr2[j]) {
+//
+//					//check if it is already taken
+//					if(visited[j] == 0) {
+//
+//						intersection.add(arr2[j]);
+//						visited[j] =1;	
+//					}
+//				}
+//				
+//				if(arr1[i] < arr2[j])
+//					break;
+//
+//			}
+//		}
+//		return intersection;
+//
+//	}
+	
+	
+	// Approach -2 
+	
 	private static List<Integer> findIntersection(int[] arr1, int[] arr2) {
-
-		int n1 = arr1.length;
-		int n2 = arr2.length;
-
-		int[] visited = new int[n2];
-
+		
+		int n1 = arr1.length , n2 = arr2.length;
+		
+		//pointers
+		int i =0, j=0;
+		
 		List<Integer> intersection = new ArrayList<Integer>();
-
-		for(int i =0 ;i<n1 ;i++) {
-
-			for(int j =0; j<n2 ; j++) {
-
-				if(arr1[i] == arr2[j]) {
-
-					//check if it is already taken
-					if(visited[j] == 0) {
-
-						intersection.add(arr2[j]);
-						visited[j] =1;	
-					}
-				}
+		
+		
+		while(i < n1 && j < n2) {
+			
+			if(arr1[i] < arr2[j]) {
+				// i is smaller move i
+				i++;
+			}else if (arr2[j] < arr1[i]) {
+				// j is smaller move j
+				j++;
+			}else {
+				// both are equal move both
 				
-				if(arr1[i] < arr2[j])
-					break;
-
+				intersection.add(arr2[j]);
+				
+				i++;
+				j++;
 			}
 		}
+		
 		return intersection;
-
 	}
 
 }
